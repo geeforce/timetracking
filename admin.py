3df75ead8b9528e5edab9afd46d4b470ab5487c0
geeforce <guidosound@gmx.de>
2018-03-15 20:05:59 +0100

    cchecko

--- admin.py
+++ admin.py
@@ -3,4 +3,9 @@ from timetracking.models import *
 
 admin.site.register(Project)
 admin.site.register(Tracking)
-admin.site.register(UserProject)
+
+class UserProjectAdmin(admin.ModelAdmin):
+	list_display = ('id','project','user')
+	list_display_links = ('id','project','user')
+	#search_fields = ('project','user')
+admin.site.register(UserProject,UserProjectAdmin)
